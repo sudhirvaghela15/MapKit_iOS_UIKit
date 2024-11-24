f51f86458a34d8d65283cf61dac4eb566f134617
Sudhir15 <sudhirvaghela15@hotmail.com>
2024-11-24 17:59:13 +0530

    Merge

---
+++ MapKit_iOS_UIKit/App/BoxBind.swift
@@ -0,0 +1,38 @@
+//
+//  Binding.swift
+//  MapKit_iOS_UIKit
+//
+//  Created by sudhir on 24/11/24.
+//
+
+import Foundation
+
+/*!
+ * @discussion  : Generic Class for store value and listern
+ * @Generic     : T
+ */
+class BoxBind<T> {
+	
+	typealias Listener = (T) -> Void
+	
+	private var listener: Listener?
+	
+	var value: T {
+		didSet {
+			listener?(value)
+		}
+	}
+	
+	init (_ value:T) {
+		self.value = value
+	}
+	
+	func bind(listener: Listener?) {
+		self.listener = listener
+	}
+	
+	func reload() {
+		self.listener?(value)
+	}
+}
+
