5fd34921e05b5948f9f8d0f1f46a30e05906d7fe
Sudhir15 <sudhirvaghela15@hotmail.com>
2024-11-25 23:16:43 +0530

    current user locatin. showing and asking location when in use permission

--- MapKit_iOS_UIKit/Modules/Map/Controller/MapViewController.swift
+++ MapKit_iOS_UIKit/Modules/Map/Controller/MapViewController.swift
@@ -6,32 +6,73 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: BaseViewController {
-	private lazy var searchController = makeSearchController()
+	
+	@IBOutlet weak var mapView: MKMapView!
+	
+	private let locationManager = CLLocationManager()
+	private var shouldUpdateLocation = true
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		_ = searchController
-		navigationController?.navigationBar.prefersLargeTitles = true
+		self.mapView.delegate = self
+		self.setupLocationManager()
+		let status = locationManager.authorizationStatus
+		debugPrint("Log - \(status.rawValue)")
 	}
 	
-	func makeSearchController() -> UISearchController {
-		let searchController = UISearchController(
-			searchResultsController: MapViewController
-				.get(viewModel: MapViewModel(title: "Address List")))
-		
-		let searchBar = searchController.searchBar
-		searchBar.sizeToFit()
-		searchBar.placeholder = "Search"
-		searchBar.searchTextField.leftView?.tintColor = UIColor.systemGray
-		searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: searchBar.placeholder ?? "", attributes: [.foregroundColor: UIColor.systemGray])
-		
-		navigationItem.titleView = searchController.searchBar
-		
-		searchController.hidesNavigationBarDuringPresentation = false
-		definesPresentationContext = true
-		return searchController
+	private func setupLocationManager() {
+		locationManager.delegate = self
+		locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+		locationManager.requestWhenInUseAuthorization()
+		locationManager.requestLocation()
+	}
+}
+
+// MARK: - MKMapViewDelegate
+extension MapViewController: MKMapViewDelegate {
+	
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		let renderer = MKPolylineRenderer(overlay: overlay)
+		renderer.strokeColor = UIColor.red.withAlphaComponent(0.65)
+		renderer.lineWidth = 4.0
+		return renderer
+	}
+	
+	func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+		guard let annotation = view.annotation else {
+			print("view.annotation is nil")
+			return
+		}
+	}
+	
+	func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+		if shouldUpdateLocation, let deviceLocation = userLocation.location {
+			shouldUpdateLocation = false
+		}
+	}
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+	private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+		if status != .authorizedWhenInUse {
+			manager.requestLocation()
+		}
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		if let location = locations.last{
+			  let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+			  let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+			  self.mapView.setRegion(region, animated: true)
+		  }
+		mapView.setUserTrackingMode(.follow, animated: true)
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+		print("manager didFailWithError: \(error.localizedDescription)")
 	}
 }
 
@@ -46,8 +87,6 @@ extension MapViewController {
 		let view = storyboard.instantiate(MapViewController.self) { coder in
 			MapViewController(coder: coder, viewModel: viewModel)
 		}
-		view.title = viewModel.title
-		
 		return view
 	}
 }
