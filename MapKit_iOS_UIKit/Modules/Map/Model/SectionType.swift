291dfa1e11f20cc3a9e4270b0351debb6678ccf7
sudhir <sudhirvaghela15@hotmail.com>
2025-02-05 23:12:14 +0530

    adding debug print nd section type enum model

---
+++ MapKit_iOS_UIKit/Modules/Map/Model/SectionType.swift
@@ -0,0 +1,14 @@
+//
+//  SectionType.swift
+//  MapKit_iOS_UIKit
+//
+//  Created by sudhir on 05/02/25.
+//
+
+import Foundation
+
+enum SectionType: Int, CaseIterable {
+		case result = 0
+		case source = 1
+		case destination = 2
+	}
