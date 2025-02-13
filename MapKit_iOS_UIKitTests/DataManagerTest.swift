3ba0de4215f458db09224ed393a9aa38a8d1643c
sudhir <sudhirvaghela15@hotmail.com>
2025-02-13 23:58:40 +0530

    adding XCTest Framwork and DataManagerTest for DataManager test cases

---
+++ MapKit_iOS_UIKitTests/DataManagerTest.swift
@@ -0,0 +1,21 @@
+//
+//  DataManagerTest.swift
+//  MapKit_iOS_UIKitTests
+//
+//  Created by sudhir on 13/02/25.
+//
+
+import XCTest
+
+final class DataManagerTest: XCTestCase {
+
+	/// current -> newPlacemark
+	/// old1 -> newPlacemark
+	/// newPlacemark -> old1
+	/// old2 -> newPlacemark
+	/// newPlacemark -> old2
+	
+	func test() {
+		XCTFail("Failing test ")
+	}
+}
