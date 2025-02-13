5a39c91f6ec5ef4e3b095f1929b9ce540addf72a
sudhir <sudhirvaghela15@hotmail.com>
2025-02-14 01:07:02 +0530

    test_fetchDirections_succeedsWhenAllRequestSucceed

--- MapKit_iOS_UIKitTests/DataManagerTest.swift
+++ MapKit_iOS_UIKitTests/DataManagerTest.swift
@@ -6,16 +6,84 @@
 //
 
 import XCTest
+import MapKit
+@testable import MapKit_iOS_UIKit
 
 final class DataManagerTest: XCTestCase {
-
-	/// current -> newPlacemark
-	/// old1 -> newPlacemark
-	/// newPlacemark -> old1
-	/// old2 -> newPlacemark
-	/// newPlacemark -> old2
 	
-	func test() {
-		XCTFail("Failing test ")
+	func test_fetchDirections_succeedsWhenAllRequestSucceed() {
+		let newPlacemark = SCPlacemark(name: "new placemark")
+	
+		/// current -> newPlacemark
+		let currentToNewPlacemarkDirection = DirectionModel(
+			source: SCPlacemark(name: "current placemark"),
+			destination: newPlacemark,
+			routes: [MKRoute()]
+		)
+		
+		/// old1 -> newPlacemark
+		let oldToNewPlacemarkDirection1 = DirectionModel(
+			source: SCPlacemark(name: "old placemark 1"),
+			destination: newPlacemark ,
+			routes: [MKRoute()]
+		)
+		
+		/// newPlacemark -> old1
+		let newToOldPlacemarkDirection1 = DirectionModel(
+			source: oldToNewPlacemarkDirection1.destination,
+			destination: oldToNewPlacemarkDirection1.source,
+			routes: [MKRoute()]
+		)
+		
+		/// old2 -> newPlacemark
+		let oldToNewPlacemarkDirection2 = DirectionModel(
+			source: SCPlacemark(name: "old placemark 2"),
+			destination: newPlacemark,
+			routes: [MKRoute()]
+		)
+		
+		/// newPlacemark -> old2
+		let newToOldPlacemarkDirection2 = DirectionModel(
+			source: oldToNewPlacemarkDirection2.destination,
+			destination: oldToNewPlacemarkDirection2 .source,
+			routes: [MKRoute()]
+		)
+		 
+		
+		let sut = DataManager { source, destination, completion in
+			completion(.success([MKRoute()]))
+		}
+		
+		let exp = expectation(description: "Wait for fetch completion")
+		
+		sut.fetchDirections(
+				ofNew: newPlacemark,
+				toOld: [oldToNewPlacemarkDirection1.source,
+						oldToNewPlacemarkDirection2.source],
+				current: currentToNewPlacemarkDirection.source) { result in
+					exp.fulfill()
+					
+					switch result {
+						case .success(let directions):
+							XCTAssertEqual(directions.count, 5)
+							XCTAssertTrue(directions.contains(currentToNewPlacemarkDirection), "missing currentToNewPlacemarkDirection")
+							XCTAssertTrue(directions.contains(oldToNewPlacemarkDirection1), "missing oldToNewPlacemarkDirection1")
+							XCTAssertTrue(directions.contains(newToOldPlacemarkDirection1), "missing newToOldPlacemarkDirection1")
+							XCTAssertTrue(directions.contains(oldToNewPlacemarkDirection2), "missing oldToNewPlacemarkDirection2")
+							XCTAssertTrue(directions.contains(newToOldPlacemarkDirection2), "missing newToOldPlacemarkDirection2")
+							/*
+							XCTAssertEqual(directions, [
+								currentToNewPlacemarkDirection,
+								oldToNewPlacemarkDirection1,
+								newToOldPlacemarkDirection1,
+								oldToNewPlacemarkDirection2,
+								newToOldPlacemarkDirection2
+							])
+							 */
+						case .failure(let failure):
+							XCTFail("Fail with error:  \(failure)")
+					}
+				}
+		wait(for: [exp], timeout: 0.5)
 	}
 }
